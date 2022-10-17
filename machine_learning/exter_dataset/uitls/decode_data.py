cc719837e404f9b294ba992930ab120d5f5eca7b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 22:05:18 +0000

    Adding tarfile member sanitization to extractall()

--- machine_learning/exter_dataset/uitls/decode_data.py
+++ machine_learning/exter_dataset/uitls/decode_data.py
@@ -104,7 +104,26 @@ def de_gz_tar(base_path,dataset_name,zip_path,output_path):
     else:
         os.mkdir(output_path)
     with tarfile.open(zip_path) as zip_ref:
-        zip_ref.extractall(path=output_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(zip_ref, path=output_path)
 
 def de_gz(base_path,dataset_name,zip_path,output_path):
     zip_path = os.path.join(base_path,"repo",dataset_name,zip_path)
--- machine_learning/exter_dataset/uitls/download.py
+++ machine_learning/exter_dataset/uitls/download.py
@@ -81,7 +81,26 @@ def gz_tar_download(base_path,dataset_name,url,name=None):
             with open(zip_name,mode="wb") as file :
                 file.write(text)
     with tarfile.open(zip_name) as zip_ref:
-        zip_ref.extractall(path=dir_name)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(zip_ref, path=dir_name)
 
 def git_download(base_path,dataset_name,url):
     
